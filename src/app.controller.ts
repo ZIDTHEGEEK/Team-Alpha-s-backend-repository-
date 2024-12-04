310f89ce972194634b22372642d3ac215ad04d8d
harrylever <ukanah15thdean@gmail.com>
2024-12-04 06:01:33 +0100

    feat: Globalize backend access for local development

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,13 @@
 import { AuthGuard } from './api/auth/auth.guard';
-import { CipherService } from './shared/cipher.service';
 import { Controller, Get, UseGuards } from '@nestjs/common';
 
 @UseGuards(AuthGuard)
 @Controller('')
 export class AppController {
-  constructor(private readonly cipherService: CipherService) {}
+  constructor() {}
 
   @Get('')
   getRoot() {
     return 'Welcome to SUI Mail API';
   }
-
-  // @RequiresAdminRole()
-  // @Get('generate-secrets')
-  // async generateServerKeys() {
-  //   const serverKeys = this.cipherService.generateUserSecretKeys();
-  //   return serverKeys;
-  // }
 }
